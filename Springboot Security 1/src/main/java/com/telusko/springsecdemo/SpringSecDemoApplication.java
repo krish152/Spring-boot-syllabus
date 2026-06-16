b8f220bc6c7036436493fa76a444cb4ba3ae6bf8
krish152 <121145660+krish152@users.noreply.github.com>
2026-06-16 21:23:44 +0530

    Add files via upload

---
+++ Springboot Security 1/src/main/java/com/telusko/springsecdemo/SpringSecDemoApplication.java
@@ -0,0 +1,12 @@
+package com.telusko.springsecdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecDemoApplication.class, args);
+	}
+}
